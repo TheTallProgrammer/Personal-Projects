12821b74ac3b849586434439e122b8805f49c908
Logan Falkenberg <loganf0101@gmail.com>
2021-09-26 12:17:19 -0500

    Created var n and associated pointer to examine how it works

--- Test-Project-00/main.cpp
+++ Test-Project-00/main.cpp
@@ -3,7 +3,30 @@
 //
 
 #include "main.h"
+
 int main(int argc, char *argv[]){
+
+    // Creating an int variable
+    int n = 5;
+    // Displays the memory address of the variable
+    std::cout << &n << std::endl;
+
+    // Creating the pointer to the memory address of int n
+    int *ptr = &n;
+
+    // This displays what the variable is pointing to, which is the address of n
+    std::cout << ptr << std::endl;
+    // This displays the value of n, putting * in front of it dereferences it
+    std::cout << *ptr << std::endl;
+
+    // Have to dereference it in order to change the value, which also changes
+    // the value that it's pointing to
+    *ptr = 10;
+    std::cout << ptr << std::endl;
+    std::cout << n << std::endl;
+    std::cout << *ptr << std::endl;
+
+
 
     return 0;
 }
