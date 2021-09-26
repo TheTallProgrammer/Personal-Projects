4b60c475f6d3c0d9964d2242a0d7571010ebe4dc
Logan Falkenberg <loganf0101@gmail.com>
2021-09-26 12:02:47 -0500

    Initial commit of test project 00

---
+++ Test-Project-00/main.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Logan on 9/26/2021.
+//
+
+#include "main.h"
+int main(int argc, char *argv[]){
+
+    return 0;
+}
---
+++ Test-Project-00/main.h
@@ -0,0 +1,10 @@
+//
+// Created by Logan on 9/26/2021.
+//
+
+#ifndef TEST_PROJECT_00_MAIN_H
+#define TEST_PROJECT_00_MAIN_H
+
+#include <iostream>
+
+#endif //TEST_PROJECT_00_MAIN_H
